5acc0a99e1462a47889295b21ac3f3df59a16b05
DogDogBird <ass7989@naver.com>
2017-12-12 21:52:11 +0900

    ADD game
    with Starcraft effect

---
+++ 블쟈런~1/Assets/Script/ObstacleManager_Starcraft.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ObstacleManager_Starcraft : MonoBehaviour {
+
+	public PlayerController thePlayer;
+	public GameManager theGameManager;
+    public GameObject bomb;
+    public GameObject explosion;
+
+    private float wait_nuclear_sound_time;
+    
+    // Use this for initialization
+    
+
+	void Start () {
+		thePlayer = GetComponent<PlayerController> ();
+	}
+
+    
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+            Time.timeScale = 0f;
+            //nuclear launch detected sound
+            Time.timeScale = 1f;
+            bomb.SetActive(true);
+            explosion.SetActive(true);
+
+
+            theGameManager.RestartGame ();//게임 터는 효과
+		}
+	}
+}
---
+++ 블쟈런~1/Assets/Script/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject pauseMenu;
+    
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("GaeGaebi Runner");
+        Time.timeScale = 1f;
+    }
+
+    public void QuitToMain()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+    }
+}
